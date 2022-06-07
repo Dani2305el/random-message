a6f0ae47d9ab809612c46abf61c9df63890668d7
alvaroku <dual.alvaro.ku@gmail.com>
2022-06-07 18:12:11 -0500

    listo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const messages = [
+    "oscar",
+    "ana",
+    "alvaro",
+    "alex",
+    "ezequiel"
+];
+const randomMsg = () =>{
+    const message = messages[Math.floor(Math.random()*messages.length)];
+    console.log(message);
+}
+module.exports ={randomMsg}
